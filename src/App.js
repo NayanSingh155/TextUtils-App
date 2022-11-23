7a507466abeb6fdab51b1329636b47eb4abcdebd
NayanSingh155 <105914443+NayanSingh155@users.noreply.github.com>
2022-11-23 21:15:30 +0530

    react pract

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+let name="Nayan"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+   <div className="App">
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+    <h1>Hello {name}</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus velit temporibus, distinctio excepturi beatae odio architecto reiciendis nisi, culpa nihil expedita totam corrupti obcaecati ipsum, quos quas nam. Qui, et.</p>
+   </div>
   );
 }
 
